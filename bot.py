78f978c96a299a0ae81147ada9af8deb35ce5a39
l3uddz <l3uddz@gmail.com>
2017-01-15 10:47:53 +0000

    added rotating log files, maximum of 5 megabytes - 5 backups.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 import config
 import manager
@@ -17,17 +18,17 @@ cfg = config.init()
 logFormatter = logging.Formatter('%(asctime)s - %(name)-20s - %(message)s')
 rootLogger = logging.getLogger()
 
-if cfg['bot.debug_file']:
-    fileHandler = logging.FileHandler('status.log')
-    fileHandler.setLevel(logging.DEBUG)
-    fileHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(fileHandler)
-
 if cfg['bot.debug_console']:
     consoleHandler = logging.StreamHandler()
     consoleHandler.setLevel(logging.DEBUG)
     consoleHandler.setFormatter(logFormatter)
     rootLogger.addHandler(consoleHandler)
+
+if cfg['bot.debug_file']:
+    fileHandler = RotatingFileHandler('status.log', maxBytes=1024 * 1024 * 5, backupCount=5)
+    fileHandler.setLevel(logging.DEBUG)
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
 
 logger = rootLogger.getChild("BOT")
 logger.setLevel(logging.DEBUG)
